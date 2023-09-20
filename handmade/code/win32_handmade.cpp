75267887a8119afef55e04fe96ab66e0b5b9a6e1
Amaan Khan <amaan7581@gmail.com>
2023-09-21 00:37:04 +0530

    created custom bitmap memory allocation

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -8,43 +8,45 @@ global_variable bool Running;
 
 global_variable BITMAPINFO BitmapInfo;
 global_variable void *BitmapMemory;
-global_variable HBITMAP BitmapHandle;
-
-global_variable HDC BitmapDeviceContext;
+global_variable int BitmapHeight;
+global_variable int BitmapWidth;
 
 
 internal void
 Win32ResizeDIBSection(int Width, int Height)
 {
-  if (BitmapHandle)
+  if(BitmapMemory)
     {
-      DeleteObject(BitmapHandle);
-    }
-  if(!BitmapDeviceContext)
-    {
-      // TODO(casey): Should we recreate these under certain special circumstances? 
-      BitmapDeviceContext = CreateCompatibleDC(0);
+      VirtualFree(BitmapMemory, 0, MEM_RELEASE); 
     }
   BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-  BitmapInfo.bmiHeader.biWidth = Width;
-  BitmapInfo.bmiHeader.biHeight = Height;
+  BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+  BitmapInfo.bmiHeader.biHeight = BitmapHeight;
   BitmapInfo.bmiHeader.biPlanes = 1;
   BitmapInfo.bmiHeader.biBitCount = 32;
   BitmapInfo.bmiHeader.biCompression = BI_RGB;
 
-  HBITMAP BitmapHandle = CreateDIBSection(BitmapDeviceContext, // TODO
-					  &BitmapInfo,   
-					  DIB_RGB_COLORS,       
-					  &BitmapMemory, 
-					  0, 0);
+  BitmapWidth = Width;
+  BitmapHeight = Height;
+
+  int BytesPerPixel = 4;
+  int BitmapMemorySize = BytesPerPixel * (Width * Height);
+
+  BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
 }
 
 internal void
-Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+Win32UpdateWindow(HDC DeviceContext, RECT *ClientRect)
 {
-  StretchDIBits(DeviceContext, 
+  int WindowWidth = ClientRect->right - ClientRect->left;
+  int WindowHeight = ClientRect->bottom - ClientRect->top;
+  StretchDIBits(DeviceContext,
+		/*
 		X, Y, Width, Height,
 		X, Y, Width, Height,
+		*/
+		0, 0, BitmapWidth, BitmapHeight,
+		0, 0, WindowWidth, WindowHeight,
 		BitmapMemory,
 		&BitmapInfo,
 		DIB_RGB_COLORS, SRCCOPY);    
@@ -93,11 +95,9 @@ Win32MainWindowCallback(HWND Window,
         HDC DeviceContext = BeginPaint(Window, &Paint);
 	
         // Do our painting here
-	int X = Paint.rcPaint.left;
-        int Y = Paint.rcPaint.top;
-        int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-        int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-	Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+	RECT ClientRect;
+	GetClientRect(Window, &ClientRect);
+	Win32UpdateWindow(DeviceContext, &ClientRect);
 	
         EndPaint(Window, &Paint);
       } break;
