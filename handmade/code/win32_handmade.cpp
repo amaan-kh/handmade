bdfecc4d389c7c17938495f09655c0cd3eb2db86
Amaan Khan <amaan7581@gmail.com>
2023-09-24 21:22:01 +0530

    added controller support (not tested and need fixing on renderer)

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -1,6 +1,7 @@
 
 #include<windows.h>
 #include<stdint.h>
+#include<xinput.h>
 
 // unsigned integers
 typedef uint8_t u8;     // 1-byte long unsigned integer
@@ -53,7 +54,64 @@ struct win32_offscreen_buffer
 //state buffer and window dimensions initialized
 global_variable bool Running;
 global_variable win32_offscreen_buffer GlobalBackbuffer;
-internal win32_window_dimension
+internal win32_window_dimension;
+
+// Create an "alias" to be able to call it with its old name
+#define XInputGetState XInputGetState_ 
+#define XInputSetState XInputSetState_
+
+// Define a type of a function
+typedef DWORD WINAPI x_input_get_state(DWORD dwUserIndex, XINPUT_STATE *pState);
+typedef DWORD WINAPI x_input_set_state(DWORD dwUserIndex, XINPUT_VIBRATION *pVibration);
+
+// Get Input State
+#define X_INPUT_GET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_STATE *pState)
+typedef X_INPUT_GET_STATE(x_input_get_state);
+X_INPUT_GET_STATE(XInputGetStateStub) 
+{ 
+    return (ERROR_DEVICE_NOT_CONNECTED); 
+}
+global_variable x_input_get_state *XInputGetState_ = XInputGetStateStub;
+
+
+// Set Input State
+#define X_INPUT_SET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_VIBRATION *pVibration)
+typedef X_INPUT_SET_STATE(x_input_set_state);
+X_INPUT_SET_STATE(XInputSetStateStub) 
+{ 
+    return (ERROR_DEVICE_NOT_CONNECTED); 
+}
+global_variable x_input_set_state *XInputSetState_ = XInputSetStateStub;
+
+
+internal void
+Win32LoadXInput()
+{
+  HMODULE XInputLibrary = LoadLibraryA("Xinput1_4.dll"); 
+  if (!XInputLibrary)
+    {
+        XInputLibrary = LoadLibraryA("Xinput1_3.dll"); 
+    }
+  if (!XInputLibrary)
+    {
+      XInputLibrary = LoadLibraryA("Xinput9_1_0.dll"); 
+    }
+  if(XInputLibrary)
+    {
+      XInputGetState = (x_input_get_state *)GetProcAddress(XInputLibrary, "XInputGetState");
+      if(!XInputGetState) { XInputGetState = XInputGetStateStub; }
+      XInputSetState = (x_input_set_state *)GetProcAddress(XInputLibrary, "XInputSetState");
+      if(!XInputSetState) { XInputSetState = XInputSetStateStub; }
+      else
+	{
+	  // We still don't have any XInputLibrary
+	  XInputGetState = XInputGetStateStub;
+	  XInputSetState = XInputSetStateStub;
+	  // TODO(casey): Diagnostic
+	}
+    }
+}
+
 
 //function that renders
 internal void
@@ -63,19 +121,9 @@ RenderWeirdGradient(win32_offscreen_buffer *Buffer, int XOffset, int YOffset)
     for (int Y = 0;
          Y < Buffer->Height;
          ++Y)
-      {
-        u32 *Pixel = (u32 *)Row;
-        for(int X = 0;
-            X < Buffer->Width;
-            ++X)
-	  {
-	    u8 Red = 0;
-	    u8 Green = (u8)(Y + YOffset);
-	    u8 Blue = (u8)(X + XOffset);
-	    *Pixel++ = Red << 16 | Green << 8 | Blue;
-	  }
-		Row += Buffer->Pitch;
-	}
+      {  
+	Row += Buffer->Pitch;
+      }
 }
 
 // allocates memory and initializes buffer
@@ -84,7 +132,7 @@ Win32ResizeDIBSection(win32_offscreen_buffer *Buffer, int Width, int Height)
 {
   if(Buffer->Memory)
     {
-      VirtualFree(Buffer->Memory, 0, MEM_RELEASE); 
+    VirtualFree(Buffer->Memory, 0, MEM_RELEASE); 
     }
   Buffer->Info.bmiHeader.biSize = sizeof(Buffer->Info.bmiHeader);
   Buffer->Info.bmiHeader.biWidth = Buffer->Width;
@@ -127,19 +175,20 @@ Win32MainWindowCallback(HWND Window,
     {
     case WM_SIZE:
       {
-	
+        win32_window_dimension Dimension = Win32GetWindowDimension(Window);
+	Win32ResizeDIBSection(&GlobalBackbuffer, Dimension.Width, Dimension.Height);
       } break;
       
     case WM_DESTROY:
       {
-	Running = false;	
+	GlobalRunning = false;	
 	OutputDebugStringA("WM_DESTROY\n");
       } break;
       
     case WM_CLOSE:
       {
 	PostQuitMessage(0);
-		Running = false;
+	GlobalRunning = false;
 	OutputDebugStringA("WM_CLOSE\n");
       } break;
       
@@ -175,6 +224,8 @@ WinMain(HINSTANCE Instance,
         int       ShowCode)
 
 {
+  //calling libraries
+  Win32LoadXInput();
   //creating a window-class
   WNDCLASSA WindowClass = {};
   WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
@@ -219,10 +270,46 @@ WinMain(HINSTANCE Instance,
 		{
 		  if (Message.message == WM_QUIT)
 		    {
-		      Running = false;
+		      GlobalRunning = false;
 		    }
 		  TranslateMessage(&Message);
 		  DispatchMessageA(&Message);
+		}
+
+	      //should this be polled more frequently 
+	      for (DWORD ControllerIndex = 0;
+		   ControllerIndex < XUSER_MAX_COUNT;
+		   ++ControllerIndex)
+		{
+		  XINPUT_STATE ControllerState;
+		  if (XInputGetState(ControllerIndex, &ControllerState) == ERROR_SUCCESS)
+		    {
+		      //controller is plugged in
+		      //see if ControllerState.dwPacketNumber increments too quickly
+		      XINPUT_GAMEPAD *Pad = &ControllerState.Gamepad;
+		      bool Up = Pad->wButtons & XINPUT_GAMEPAD_DPAD_UP;
+		      bool Down          = Pad->wButtons & XINPUT_GAMEPAD_DPAD_DOWN;
+		      bool Left          = Pad->wButtons & XINPUT_GAMEPAD_DPAD_LEFT;
+		      bool Right         = Pad->wButtons & XINPUT_GAMEPAD_DPAD_RIGHT;
+		      bool Start         = Pad->wButtons & XINPUT_GAMEPAD_START;
+		      bool Back          = Pad->wButtons & XINPUT_GAMEPAD_BACK;
+		      bool LeftShoulder  = Pad->wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER;
+		      bool RightShoulder = Pad->wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER;
+		      bool A             = Pad->wButtons & XINPUT_GAMEPAD_A;
+		      bool B             = Pad->wButtons & XINPUT_GAMEPAD_B;
+		      bool X             = Pad->wButtons & XINPUT_GAMEPAD_X;
+		      bool Y             = Pad->wButtons & XINPUT_GAMEPAD_Y;
+		      s16 StickX = Pad->sThumbLX;
+		      s16 StickY = Pad->sThumbLY;
+		      if(A)
+			{
+			  YOffset += 2;
+			}
+		    }
+		  else
+		    {
+		      // controller is not available
+		    }
 		}
 	      
 	      //rendering 
